73b574d96d84eefa9bcbdc319ef9faca0686279a
crystalDf <chendong333@gmail.com>
2016-10-05 16:52:51 +0800

    Upgrade to API Level 24

--- app/src/main/java/com/star/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/star/beatbox/BeatBoxFragment.java
@@ -2,7 +2,6 @@ package com.star.beatbox;
 
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -22,13 +21,12 @@ public class BeatBoxFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mBeatBox = new BeatBox(getActivity());
     }
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_beat_box, container, false);
